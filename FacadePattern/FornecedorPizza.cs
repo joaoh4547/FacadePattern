a3540d2c9e1ab17273be9efef936bd48287bdab7
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 20:11:09 -0300

    add FornecedorPizza

---
+++ FacadePattern/FornecedorPizza.cs
@@ -0,0 +1,24 @@
+﻿using FacadePattern.Interfaces;
+using System;
+
+namespace FacadePattern
+{
+    public class FornecedorPizza : IPizza
+    {
+        public void BuscarPizzaNormal()
+        {
+            ObterCoberturaNaoVegana();
+            Console.WriteLine("Buscando Pizza Normal");
+        }
+
+        public void BuscarPizzaVegana()
+        {
+            Console.WriteLine("Busacando Pizza vegana");
+        }
+
+        private void ObterCoberturaNaoVegana()
+        {
+            Console.WriteLine("Obtendo coberturas de pizza não veganas.");
+        }
+    }
+}
