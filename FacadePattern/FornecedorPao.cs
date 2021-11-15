b0053737492c5c8a3c9411455137b200f786801e
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 20:25:53 -0300

    add RestaurenteFacade

---
+++ FacadePattern/FornecedorPao.cs
@@ -0,0 +1,23 @@
+﻿using FacadePattern.Interfaces;
+using System;
+
+namespace FacadePattern
+{
+    public class FornecedorPao : IPao
+    {
+        public void BuscarPaoDeAlho()
+        {
+            Console.WriteLine("Obtendo Pão de alho");
+        }
+
+        public void BuscarPaoDeAlhoComQueijo()
+        {
+            ObterQueijo();
+            Console.WriteLine("Obtendo Pão de alho com queijo");
+        }
+        private void ObterQueijo()
+        {
+            Console.WriteLine("Obtendo Queijo");
+        }
+    }
+}
---
+++ FacadePattern/RestaurenteFacade.cs
@@ -0,0 +1,41 @@
+﻿using FacadePattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class RestaurenteFacade
+    {
+        private IPizza _fornecedorPizza;
+        private IPao _fornecedorPao;
+
+        public RestaurenteFacade()
+        {
+            _fornecedorPizza = new FornecedorPizza();
+            _fornecedorPao = new FornecedorPao();
+        }
+
+        public void BuscarPizzaNormal()
+        {
+            _fornecedorPizza.BuscarPizzaNormal();
+        }
+
+        public void BuscarPizzaVegana()
+        {
+            _fornecedorPizza.BuscarPizzaVegana();
+        }
+
+        public void BuscarPaoDeAlho()
+        {
+            _fornecedorPao.BuscarPaoDeAlho();
+        }
+
+        public void BuscarPaoDeAlhoComQueijo()
+        {
+            _fornecedorPao.BuscarPaoDeAlhoComQueijo();
+        }
+    }
+}
